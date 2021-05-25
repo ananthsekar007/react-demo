b2ffe4e369cf0ad0881fd064aad56e2b9280439f
Ananth Sekar <ananth.sekar@gogosoon.com>
2021-05-25 09:37:32 +0530

    [Ananth] Adds local storage functionality

--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,13 @@
-import React , {useState} from "react";
+import React , {useState, useEffect} from "react";
 
 export default function Todo() {
     const [value, setValue] = useState("");
     const [todo, setTodo] = useState([]);
+
+    useEffect(() => {
+        let array = JSON.parse(localStorage.getItem("array"));
+        setTodo(array);
+    })
 
     const addText = () => {
         let array1 = todo; // this declaration refers  the memory
@@ -10,13 +15,14 @@ export default function Todo() {
         array.push(value);
         setValue("");
         setTodo(array);
-        console.log({array});
-    } 
+        localStorage.setItem("array", JSON.stringify(array));
+    }
 
     const deleteLast = ()=> {
         let array = [...todo];
         array.pop();
         setTodo(array);
+        localStorage.setItem("array", JSON.stringify(array));
         console.log(array);
     }
 
@@ -24,6 +30,7 @@ export default function Todo() {
         let array = [...todo];
         array.splice(index, 1);
         setTodo(array);
+        localStorage.setItem("array", JSON.stringify(array));
     }
 
     const editElement = (index) => {
@@ -31,6 +38,7 @@ export default function Todo() {
         let value = prompt("Enter the replacement value");
         array.splice(index, 1, value);
         setTodo(array);
+        localStorage.setItem("array", JSON.stringify(array));   
         console.log(array);
     }
 
